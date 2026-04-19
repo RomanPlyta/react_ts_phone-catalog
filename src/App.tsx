c75805873eb7d09402f70c85b4151807e492a790
Admin <romanpluta42@gmail.com>
2026-04-19 18:36:28 +0300

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import { Header } from './components/Header';
+import { HomePage } from './pages/HomePage';
+import { PhonesPage } from './pages/PhonesPage';
+import { TabletsPage } from './pages/TabletsPage';
+import { AccessoriesPage } from './pages/AccessoriesPage';
+import { ProductDetailsPage } from './pages/ProductDetailsPage';
+import { Footer } from './components/Footer';
+import { CartPage } from './pages/CartPage';
+import { FavoritesPage } from './pages/FavoritesPage';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="App">
+      <Header />
+
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/phones" element={<PhonesPage />} />
+          <Route path="/phones/:productId" element={<ProductDetailsPage />} />
+
+          <Route path="/tablets" element={<TabletsPage />} />
+          <Route path="/tablets/:productId" element={<ProductDetailsPage />} />
+
+          <Route path="/accessories" element={<AccessoriesPage />} />
+          <Route
+            path="/accessories/:productId"
+            element={<ProductDetailsPage />}
+          />
+
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/cart" element={<CartPage />} />
+
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </main>
+
+      <Footer />
     </div>
   );
 };
+
+export default App;
